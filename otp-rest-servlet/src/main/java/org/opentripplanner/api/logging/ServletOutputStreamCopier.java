3527304ca5f5c51c0a77247b5de92a1b3beebc4b
Zsombor Welker <flaktack@welker.hu>
2013-07-26 10:33:42 +0200

    utazástervezések loggolása

---
+++ otp-rest-servlet/src/main/java/org/opentripplanner/api/logging/ServletOutputStreamCopier.java
@@ -0,0 +1,40 @@
+/* This program is free software: you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public License
+ as published by the Free Software Foundation, either version 3 of
+ the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+package org.opentripplanner.api.logging;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.servlet.ServletOutputStream;
+
+public class ServletOutputStreamCopier extends ServletOutputStream {
+
+    private OutputStream outputStream;
+    private ByteArrayOutputStream copy;
+
+    public ServletOutputStreamCopier(OutputStream outputStream) {
+        this.outputStream = outputStream;
+        this.copy = new ByteArrayOutputStream(1024 * 1024);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        outputStream.write(b);
+        copy.write(b);
+    }
+
+    public byte[] getCopy() {
+        return copy.toByteArray();
+    }
+}
