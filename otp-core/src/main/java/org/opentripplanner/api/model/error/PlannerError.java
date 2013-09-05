4238ab07ebf9f52a8a318303d005f4924124f438
Zsombor Welker <flaktack@welker.hu>
2013-09-05 08:25:15 +0200

    Don't log common errors when planning trips

--- otp-core/src/main/java/org/opentripplanner/api/model/error/PlannerError.java
+++ otp-core/src/main/java/org/opentripplanner/api/model/error/PlannerError.java
@@ -131,4 +131,8 @@ public class PlannerError {
     public boolean getNoPath() {
         return noPath;
     }
+    
+    public static boolean isPlanningError(Class<?> clazz) {
+        return messages.containsKey(clazz);
+    }
 }
