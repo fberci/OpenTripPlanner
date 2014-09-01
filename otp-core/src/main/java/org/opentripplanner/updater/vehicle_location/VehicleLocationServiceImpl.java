868ab70f17370f92267466e9c8b046d1ffc6cf69
Zsombor Welker <flaktack@welker.hu>
2014-09-01 15:31:42 +0200

    oba api - ignore vehicles older than 300 seconds

--- otp-core/src/main/java/org/opentripplanner/updater/vehicle_location/VehicleLocationServiceImpl.java
+++ otp-core/src/main/java/org/opentripplanner/updater/vehicle_location/VehicleLocationServiceImpl.java
@@ -13,6 +13,9 @@
 
 package org.opentripplanner.updater.vehicle_location;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.index.quadtree.Quadtree;
 import lombok.Setter;
@@ -29,162 +32,177 @@ import java.util.List;
 import java.util.Map;
 
 public class VehicleLocationServiceImpl implements VehicleLocationService {
-    
-    private long _lastUpdateTime = Long.MIN_VALUE;
-    
-    @Setter
-    private Graph graph;
-    
-    private Quadtree _quadtree = new Quadtree();
-    
-    private Map<AgencyAndId, VehicleLocation> _vehicleLocations = new HashMap<AgencyAndId, VehicleLocation>();
-    
-    private Map<AgencyAndId, VehicleLocation> _vehicleLocationsByTrip = new HashMap<AgencyAndId, VehicleLocation>();
-    
-    private Map<AgencyAndId, List<VehicleLocation>> _vehicleLocationsByRoute = new HashMap<AgencyAndId, List<VehicleLocation>>();
-    
-    private Map<String, List<VehicleLocation>> _vehicleLocationsByAgency = new HashMap<String, List<VehicleLocation>>();
 
-    @Override
-    public VehicleLocation getForVehicle(AgencyAndId vehicleId) {
-        return _vehicleLocations.get(vehicleId);
-    }
+	private static final long VEHICLE_MAX_AGE = 300;
 
-    @Override
-    public VehicleLocation getForTrip(AgencyAndId tripId) {
-        return _vehicleLocationsByTrip.get(tripId);
-    }
+	private long _lastUpdateTime = Long.MIN_VALUE;
 
-    @Override
-    public List<VehicleLocation> getForRoute(AgencyAndId routeId) {
-        List<VehicleLocation> locations = _vehicleLocationsByRoute.get(routeId);
-        if(locations == null)
-            return Collections.emptyList();
-        return locations;
-    }
+	@Setter
+	private Graph graph;
 
-    @Override
-    public List<VehicleLocation> getForAgency(String agencyId) {
-        List<VehicleLocation> locations = _vehicleLocationsByAgency.get(agencyId);
-        if(locations == null)
-            return Collections.emptyList();
-        return locations;
-    }
+	private Quadtree _quadtree = new Quadtree();
 
-    @Override
-    public List<VehicleLocation> getForArea(Envelope area) {
-        List maybeInside = _quadtree.query(area);
-        
-        List<VehicleLocation> vehicles = new LinkedList<VehicleLocation>();
-        for(Object o : maybeInside) {
-            VehicleLocation vehicle = (VehicleLocation) o;
-            if(area.contains(vehicle.getCoordinate()))
-                vehicles.add(vehicle);
-        }
-        
-        return vehicles;
-    }
-    
-    @Override
-    public Collection<VehicleLocation> getAll() {
-        return _vehicleLocations.values();
-    }
+	private Map<AgencyAndId, VehicleLocation> _vehicleLocations = new HashMap<AgencyAndId, VehicleLocation>();
 
-    @Override
-    public boolean add(VehicleLocation vehicle) {
-        if(_vehicleLocations.containsKey(vehicle.getVehicleId())) {
-            VehicleLocation existingLocation = _vehicleLocations.get(vehicle.getVehicleId());
-            if(vehicle.getTimestamp() <= existingLocation.getTimestamp())
-                return false;
-            
-            remove(vehicle.getVehicleId());
+	private Map<AgencyAndId, VehicleLocation> _vehicleLocationsByTrip = new HashMap<AgencyAndId, VehicleLocation>();
+
+	private Map<AgencyAndId, List<VehicleLocation>> _vehicleLocationsByRoute = new HashMap<AgencyAndId, List<VehicleLocation>>();
+
+	private Map<String, List<VehicleLocation>> _vehicleLocationsByAgency = new HashMap<String, List<VehicleLocation>>();
+
+	@Override
+	public VehicleLocation getForVehicle(AgencyAndId vehicleId) {
+		VehicleLocation vehicle = _vehicleLocations.get(vehicleId);
+		return VEHICLE_FILTER.apply(vehicle) ? vehicle : null;
+	}
+
+	@Override
+	public VehicleLocation getForTrip(AgencyAndId tripId) {
+		VehicleLocation vehicle = _vehicleLocationsByTrip.get(tripId);
+		return VEHICLE_FILTER.apply(vehicle) ? vehicle : null;
+	}
+
+	@Override
+	public List<VehicleLocation> getForRoute(AgencyAndId routeId) {
+		List<VehicleLocation> locations = _vehicleLocationsByRoute.get(routeId);
+		if (locations == null)
+			return Collections.emptyList();
+		return locations;
+	}
+
+	@Override
+	public List<VehicleLocation> getForAgency(String agencyId) {
+		List<VehicleLocation> vehicles = _vehicleLocationsByAgency.get(agencyId);
+		if (vehicles == null)
+			return Collections.emptyList();
+
+		return Lists.newArrayList(Iterables.filter(vehicles, VEHICLE_FILTER));
+	}
+
+	@Override
+	public List<VehicleLocation> getForArea(Envelope area) {
+		List maybeInside = _quadtree.query(area);
+
+		List<VehicleLocation> vehicles = new LinkedList<VehicleLocation>();
+		for (Object o : maybeInside) {
+			VehicleLocation vehicle = (VehicleLocation) o;
+			if (area.contains(vehicle.getCoordinate()))
+				vehicles.add(vehicle);
+		}
+
+		return Lists.newArrayList(Iterables.filter(vehicles, VEHICLE_FILTER));
+	}
+
+	@Override
+	public Collection<VehicleLocation> getAll() {
+		return _vehicleLocations.values();
+	}
+
+	@Override
+	public boolean add(VehicleLocation vehicle) {
+		if (_vehicleLocations.containsKey(vehicle.getVehicleId())) {
+			VehicleLocation existingLocation = _vehicleLocations.get(vehicle.getVehicleId());
+			if (vehicle.getTimestamp() <= existingLocation.getTimestamp())
+				return false;
+
+			remove(vehicle.getVehicleId());
             
             /*if(!existingLocation.getCoordinate().equals(vehicle.getCoordinate()))
                 vehicle.setBearing(new Float(DirectionUtils.getAzimuth(existingLocation.getCoordinate(), vehicle.getCoordinate())));
             else
                 vehicle.setBearing(existingLocation.getBearing());*/
-        }
-        
-        TransitIndexService transitIndexService = graph.getService(TransitIndexService.class);
-        AgencyAndId routeId = vehicle.getRouteId();
-        if(vehicle.getTripId() != null)
-            routeId = transitIndexService.getTripPatternForTrip(vehicle.getTripId(), vehicle.getServiceDate()).getExemplar().getRoute().getId();
-        
-        _lastUpdateTime = System.currentTimeMillis() / 1000;
+		}
 
-        _quadtree.insert(new Envelope(vehicle.getCoordinate()), vehicle);
-        _vehicleLocations.put(vehicle.getVehicleId(), vehicle);
-        MapUtils.addToMapList(_vehicleLocationsByAgency, vehicle.getVehicleId().getAgencyId(), vehicle);
-        if(vehicle.getTripId() != null)
-            _vehicleLocationsByTrip.put(vehicle.getTripId(), vehicle);
-        if(routeId != null)
-            MapUtils.addToMapList(_vehicleLocationsByRoute, routeId, vehicle);
-        
-        return true;
-    }
+		TransitIndexService transitIndexService = graph.getService(TransitIndexService.class);
+		AgencyAndId routeId = vehicle.getRouteId();
+		if (vehicle.getTripId() != null)
+			routeId = transitIndexService.getTripPatternForTrip(vehicle.getTripId(), vehicle.getServiceDate()).getExemplar().getRoute().getId();
 
-    @Override
-    public VehicleLocation remove(AgencyAndId vehicleId) {
-        if(!_vehicleLocations.containsKey(vehicleId))
-            return null;
-        
-        VehicleLocation vehicle = _vehicleLocations.get(vehicleId);
-        
-        TransitIndexService transitIndexService = graph.getService(TransitIndexService.class);
-        AgencyAndId routeId = null;
-        if(vehicle.getTripId() != null)
-            routeId = transitIndexService.getTripPatternForTrip(vehicle.getTripId(), vehicle.getServiceDate()).getExemplar().getRoute().getId();
-        
-        _quadtree.remove(new Envelope(vehicle.getCoordinate()), vehicle);
-        _vehicleLocations.remove(vehicleId);
-        MapUtils.removeFromMapList(_vehicleLocationsByAgency, vehicle.getVehicleId().getAgencyId(), vehicle);
-        if(vehicle.getTripId() != null)
-            _vehicleLocationsByTrip.remove(vehicle.getTripId());
-        if(routeId != null)
-            MapUtils.removeFromMapList(_vehicleLocationsByRoute, routeId, vehicle);
-        
-        return vehicle;
-    }
+		_lastUpdateTime = System.currentTimeMillis() / 1000;
 
-    @Override
-    public void refresh(Collection<VehicleLocation> updatedLocations) {
-        TransitIndexService transitIndexService = graph.getService(TransitIndexService.class);
-        
-        Quadtree quadtree = new Quadtree();
-        Map<AgencyAndId, VehicleLocation> vehicleLocations = new HashMap<AgencyAndId, VehicleLocation>();
-        Map<AgencyAndId, VehicleLocation> vehicleLocationsByTrip = new HashMap<AgencyAndId, VehicleLocation>();
-        Map<AgencyAndId, List<VehicleLocation>> vehicleLocationsByRoute = new HashMap<AgencyAndId, List<VehicleLocation>>();
-        Map<String, List<VehicleLocation>> vehicleLocationsByAgency = new HashMap<String, List<VehicleLocation>>();
-        
-        long maxTime = Long.MIN_VALUE;
-        
-        for(VehicleLocation vehicle : updatedLocations) {
-            AgencyAndId routeId = vehicle.getRouteId();
-            if(vehicle.getTripId() != null)
-                routeId = transitIndexService.getTripPatternForTrip(vehicle.getTripId(), vehicle.getServiceDate()).getExemplar().getRoute().getId();
+		_quadtree.insert(new Envelope(vehicle.getCoordinate()), vehicle);
+		_vehicleLocations.put(vehicle.getVehicleId(), vehicle);
+		MapUtils.addToMapList(_vehicleLocationsByAgency, vehicle.getVehicleId().getAgencyId(), vehicle);
+		if (vehicle.getTripId() != null)
+			_vehicleLocationsByTrip.put(vehicle.getTripId(), vehicle);
+		if (routeId != null)
+			MapUtils.addToMapList(_vehicleLocationsByRoute, routeId, vehicle);
 
-            quadtree.insert(new Envelope(vehicle.getCoordinate()), vehicle);
-            vehicleLocations.put(vehicle.getVehicleId(), vehicle);
-            MapUtils.addToMapList(vehicleLocationsByAgency, vehicle.getVehicleId().getAgencyId(), vehicle);
-            if(vehicle.getTripId() != null)
-                vehicleLocationsByTrip.put(vehicle.getTripId(), vehicle);
-            if(routeId != null)
-                MapUtils.addToMapList(vehicleLocationsByRoute, routeId, vehicle);
-            
-            if(maxTime < vehicle.getTimestamp())
-                maxTime = vehicle.getTimestamp();
-        }
-        
-        _lastUpdateTime = System.currentTimeMillis() / 1000;
-        _quadtree = quadtree;
-        _vehicleLocations = vehicleLocations;
-        _vehicleLocationsByTrip = vehicleLocationsByTrip;
-        _vehicleLocationsByRoute = vehicleLocationsByRoute;
-        _vehicleLocationsByAgency = vehicleLocationsByAgency;
-    }
+		return true;
+	}
 
-    @Override
-    public long getLastUpdateTime() {
-        return _lastUpdateTime;
-    }
+	@Override
+	public VehicleLocation remove(AgencyAndId vehicleId) {
+		if (!_vehicleLocations.containsKey(vehicleId))
+			return null;
+
+		VehicleLocation vehicle = _vehicleLocations.get(vehicleId);
+
+		TransitIndexService transitIndexService = graph.getService(TransitIndexService.class);
+		AgencyAndId routeId = null;
+		if (vehicle.getTripId() != null)
+			routeId = transitIndexService.getTripPatternForTrip(vehicle.getTripId(), vehicle.getServiceDate()).getExemplar().getRoute().getId();
+
+		_quadtree.remove(new Envelope(vehicle.getCoordinate()), vehicle);
+		_vehicleLocations.remove(vehicleId);
+		MapUtils.removeFromMapList(_vehicleLocationsByAgency, vehicle.getVehicleId().getAgencyId(), vehicle);
+		if (vehicle.getTripId() != null)
+			_vehicleLocationsByTrip.remove(vehicle.getTripId());
+		if (routeId != null)
+			MapUtils.removeFromMapList(_vehicleLocationsByRoute, routeId, vehicle);
+
+		return vehicle;
+	}
+
+	@Override
+	public void refresh(Collection<VehicleLocation> updatedLocations) {
+		TransitIndexService transitIndexService = graph.getService(TransitIndexService.class);
+
+		Quadtree quadtree = new Quadtree();
+		Map<AgencyAndId, VehicleLocation> vehicleLocations = new HashMap<AgencyAndId, VehicleLocation>();
+		Map<AgencyAndId, VehicleLocation> vehicleLocationsByTrip = new HashMap<AgencyAndId, VehicleLocation>();
+		Map<AgencyAndId, List<VehicleLocation>> vehicleLocationsByRoute = new HashMap<AgencyAndId, List<VehicleLocation>>();
+		Map<String, List<VehicleLocation>> vehicleLocationsByAgency = new HashMap<String, List<VehicleLocation>>();
+
+		long maxTime = Long.MIN_VALUE;
+
+		for (VehicleLocation vehicle : updatedLocations) {
+			AgencyAndId routeId = vehicle.getRouteId();
+			if (vehicle.getTripId() != null)
+				routeId = transitIndexService.getTripPatternForTrip(vehicle.getTripId(), vehicle.getServiceDate()).getExemplar().getRoute().getId();
+
+			quadtree.insert(new Envelope(vehicle.getCoordinate()), vehicle);
+			vehicleLocations.put(vehicle.getVehicleId(), vehicle);
+			MapUtils.addToMapList(vehicleLocationsByAgency, vehicle.getVehicleId().getAgencyId(), vehicle);
+			if (vehicle.getTripId() != null)
+				vehicleLocationsByTrip.put(vehicle.getTripId(), vehicle);
+			if (routeId != null)
+				MapUtils.addToMapList(vehicleLocationsByRoute, routeId, vehicle);
+
+			if (maxTime < vehicle.getTimestamp())
+				maxTime = vehicle.getTimestamp();
+		}
+
+		_lastUpdateTime = System.currentTimeMillis() / 1000;
+		_quadtree = quadtree;
+		_vehicleLocations = vehicleLocations;
+		_vehicleLocationsByTrip = vehicleLocationsByTrip;
+		_vehicleLocationsByRoute = vehicleLocationsByRoute;
+		_vehicleLocationsByAgency = vehicleLocationsByAgency;
+	}
+
+	@Override
+	public long getLastUpdateTime() {
+		return _lastUpdateTime;
+	}
+
+	private static final VehicleFilter VEHICLE_FILTER = new VehicleFilter();
+	private static class VehicleFilter implements Predicate<VehicleLocation> {
+
+		@Override
+		public boolean apply(VehicleLocation input) {
+			return input != null && input.getTimestamp() + VEHICLE_MAX_AGE > System.currentTimeMillis() / 1000;
+		}
+	}
 }
+
