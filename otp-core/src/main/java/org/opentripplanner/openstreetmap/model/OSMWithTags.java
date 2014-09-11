06e69016c07bd88c31f96402c48adc8e129ea9e8
Zsombor Welker <flaktack@welker.hu>
2014-09-11 13:40:38 +0200

    OSM - treat throughTrafficDisallowed ways as accessDenied ways

--- otp-core/src/main/java/org/opentripplanner/openstreetmap/model/OSMWithTags.java
+++ otp-core/src/main/java/org/opentripplanner/openstreetmap/model/OSMWithTags.java
@@ -202,7 +202,7 @@ public class OSMWithTags {
      */
     private boolean isTagDeniedAccess(String tagName) {
         String tagValue = getTag(tagName);
-        return "no".equals(tagValue) || "license".equals(tagValue);
+        return "no".equals(tagValue) || "license".equals(tagValue) || isThroughTrafficExplicitlyDisallowed();
     }
 
     /**
